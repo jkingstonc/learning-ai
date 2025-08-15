182c06e79180b3955244c6193b03213bf2f72fb4
James Clarke <j.kingston.c@gmail.com>
2025-08-15 21:40:53 +0100

    added pydantic AI example

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from learning-ai!")
+from pydantic_ai import Agent, RunContext
+from pydantic import BaseModel
+from dataclasses import dataclass
 
 
-if __name__ == "__main__":
-    main()
+class Result(BaseModel):
+    year: int
+    author: str
+
+
+@dataclass
+class Params:
+    programming_language: str
+
+
+agent = Agent(
+    "google-gla:gemini-1.5-flash",
+    deps_type=Params,
+    output_type=Result,
+)
+
+
+@agent.system_prompt
+def get_system_prompt(ctx: RunContext[Params]) -> str:
+    print(f"System prompt called with deps: {ctx.deps}")
+    return f"You are a knowledge base for the following programming language: {ctx.deps.programming_language}"
+
+
+result = agent.run_sync(
+    "What year was the language released released?",
+    deps=Params(programming_language="Rust"),
+)
+print(result.output.year)
+print(result.output.author)
